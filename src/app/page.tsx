4152790ce03bb2ad00b45986d192a5a25c76e21c
correojago <correojago@gmail.com>
2026-04-26 08:55:24 +0000

    Fix Zapier chatbot using next/script correctly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,6 +31,7 @@ import { Droplets, Building, Mail, Lock, AlertCircle, ArrowRight, ArrowLeft, Pho
 import PricingSection from '@/components/PricingSection';
 import FAQSection from '@/components/FAQSection';
 import HowItWorksSection from '@/components/HowItWorksSection';
+import Script from 'next/script';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://vhvynlhbgpittimyopue.supabase.co';
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhodnluaGhiZ3BpdHRpbXlvcHVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMyOTM3NTYsImV4cCI6MjA1ODg2OTc1Nn0.sb_publishable_ZINHGD4RZ1cPw2yIHcokxQ_MVlyMO-Z';
@@ -1722,15 +1723,19 @@ export default function HomePage() {
       </footer>
 
       {/* Zapier Chatbot */}
-      <div dangerouslySetInnerHTML={{ __html: `
-        <script async type='module' src='https://interfaces.zapier.com/assets/web-components/zapier-interfaces/zapier-interfaces.esm.js'></script>
-        <zapier-interfaces-chatbot-embed 
-          is-popup='true' 
-          chatbot-id='cmoby1wx90033arnq1e04imh7' 
-          height='600px' 
-          width='400px'>
-        </zapier-interfaces-chatbot-embed>
-      `}} />
+      <Script 
+        src="https://interfaces.zapier.com/assets/web-components/zapier-interfaces/zapier-interfaces.esm.js" 
+        type="module"
+        strategy="afterInteractive"
+      />
+      
+      {/* @ts-ignore */}
+      <zapier-interfaces-chatbot-embed 
+        is-popup="true" 
+        chatbot-id="cmoby1wx90033arnq1e04imh7" 
+        height="600px" 
+        width="400px"
+      />
 
     </div>
   );
